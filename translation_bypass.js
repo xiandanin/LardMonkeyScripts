88e38af99c298a9e151109a733867fb46d56f5d5
Xian Dan <denghahae@gmail.com>
2020-09-05 23:27:57 +0800

    适配最新github页面

--- translation_bypass.js
+++ translation_bypass.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         谷歌翻译绕过代码块
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  让谷歌翻译插件翻译网页的时候，绕过代码块和一些无需翻译的元素
 // @author       xiandan
 // @homeurl      https://github.com/xiandanin/LardMonkeyScripts
@@ -17,20 +17,43 @@
 (function () {
     'use strict'
 
+    function noTranslate (array) {
+        array.forEach((name) => {
+            [...document.querySelectorAll(name)].forEach(node => {
+                if (node.className.indexOf('notranslate') === -1) {
+                    node.classList.add('notranslate')
+                }
+            })
+        })
+    }
+
     const bypassSelectorArray = [
         'pre'
     ]
-    if (window.location.hostname.indexOf("github")!==-1) {
+    if (window.location.hostname.indexOf("github") !== -1) {
         // 如果是github 还需要处理一些别的元素
-        const githubSelector = ['.pagehead', '.octotree-sidebar', '.repository-topics-container', '.file-navigation', '.Box-title', '.content']
+        const githubSelector = [
+            '.bg-gray-light.pt-3.hide-full-screen.mb-5',
+            '.octotree-sidebar',
+            'summary.btn.css-truncate',
+            '.commit-author',
+            '.js-navigation-open.link-gray-dark',
+            '.Box-title',
+            '.BorderGrid-cell > div.mt-3 > a.muted-link',
+            '.BorderGrid-cell > ul.list-style-none'
+        ]
         bypassSelectorArray.push.apply(bypassSelectorArray, githubSelector)
-    }
 
-    bypassSelectorArray.forEach((name) => {
-        [...document.querySelectorAll(name)].forEach(node => {
-            if (node.className.indexOf('notranslate') === -1) {
-                node.classList.add('notranslate')
-            }
-        })
-    })
+        //如果还有github的插件 还需要延迟追加一些
+        setTimeout(function () {
+            const githubPluginSelector = [
+                '.github-repo-size-div',
+                '.octotree-sidebar'
+            ]
+            noTranslate(githubPluginSelector)
+        }, 3000)
+    }
+    noTranslate(bypassSelectorArray)
+
+
 })()
