b4474502a14d7c260ef674d37470b38bd00a579a
xiandanin <denghahae@gmail.com>
2021-05-08 17:55:30 +0800

    适配新的google页面

--- hide_search_history.js
+++ hide_search_history.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         隐藏搜索历史
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  隐藏搜索引擎下拉框的历史记录，目前仅适配Google
 // @homeurl      https://github.com/dengyuhan/LardMonkeyScripts
 // @homeurl      https://greasyfork.org/zh-CN/scripts/392368
@@ -16,24 +16,36 @@
 (function () {
     'use strict'
 
-    function findTagByParent (element, tagName) {
-        let currentElement = element
-        while (currentElement.tagName.toLowerCase() !== tagName.toLowerCase() && currentElement !== document.body) {
-            currentElement = currentElement.parentNode
-        }
-        return currentElement !== element ? currentElement : null
-    }
+    const deleteCache = []
 
     function deleteFunc () {
-        let deleteElements = Array.from(document.getElementsByClassName("sbai sbdb"))
-        deleteElements.filter(element => {
-            return element.style.display !== 'none'
-        }).flatMap(element => {
-            return findTagByParent(element, 'li')
-        }).forEach(element => {
-            console.info("删除搜索记录", element.querySelector("span").innerText)
-            element.remove()
+        // 删除按钮的元素
+        let elements = document.querySelectorAll(".sbai");
+        let deleteElements = []
+        let deleteParentElement = []
+        elements.forEach(function (element) {
+            // 没隐藏的说明可以删除
+            if (element.parentElement.style.display !== 'none') {
+                let parentElement = element.parentElement.parentElement;
+                let textElement = parentElement.querySelector('.zRAHie');
+                let textContent = textElement.textContent;
+                if (textElement && textContent && deleteCache.indexOf(textContent) === -1) {
+                    deleteElements.push(element)
+                    deleteParentElement.push(parentElement)
+
+                    deleteCache.push(textContent)
+                }
+            }
         })
+        console.info("可删除元素%d个", deleteElements.length)
+        for (let i = 0; i < deleteElements.length; i++) {
+            deleteParentElement[i].style.display = 'none'
+            deleteElements[i].click()
+            setTimeout(function () {
+                deleteParentElement[i].style.display = ''
+            }, 200)
+            console.info("删除搜索记录", deleteParentElement[i], deleteParentElement[i].textContent)
+        }
     }
 
     let inputElements = document.getElementsByName("q")
@@ -43,14 +55,16 @@
         }
     }
 
-    let target = document.getElementsByClassName("UUbT9")[0]
-    let observer = new MutationObserver(function (mutations, observer) {
-        deleteFunc()
-    })
-    observer.observe(target, {
-        characterData: true,
-        childList: true,
-        subtree: true
-    })
+    let target = document.querySelector(".UUbT9")
+    if (target != null) {
+        let observer = new MutationObserver(function (mutations, observer) {
+            deleteFunc()
+        })
+        observer.observe(target, {
+            characterData: true,
+            childList: true,
+            subtree: true
+        })
+    }
 
 })()
